a48a163c644e86ddf2cc64906efadff5a4c06351
Sam Fober <samfober@hotmail.nl>
2024-10-28 01:48:37 +0100

    Initial commit

---
+++ src/main/kotlin/nl/avans/Application.kt
@@ -0,0 +1,16 @@
+package nl.avans
+
+import io.ktor.server.application.*
+import nl.avans.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
